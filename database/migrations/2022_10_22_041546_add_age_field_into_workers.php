6f7f1ada24f7a765d657d59151bff247fd6eae09
rfv234 <gromdusha154@gmail.com>
2022-10-22 11:31:09 +0700

    Новые миграции

---
+++ database/migrations/2022_10_22_041546_add_age_field_into_workers.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAgeFieldIntoWorkers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('workers', function (Blueprint $table) {
+            $table->integer('age')->after('name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
