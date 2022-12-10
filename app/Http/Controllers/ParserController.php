6bb81847d24511b505fc931d9c105a7b76f69f70
rfv234 <gromdusha154@gmail.com>
2022-12-10 11:31:31 +0700

    new parses

--- app/Http/Controllers/ParserController.php
+++ app/Http/Controllers/ParserController.php
@@ -39,6 +39,41 @@ class ParserController extends Controller
         $htmlCode = file_get_contents($item['loc']);
         $crawler = new Crawler($htmlCode, $item['loc']);
         $name = $crawler->filter('h1')->text();
-        dd($name);
+        try {
+            $description = $crawler->filter('div.item-description')->html();
+        } catch (\Exception $e) {
+            $description = '';
+        }
+        $category = $crawler->filter('ul.breadcrumbs >li >a')->last()->attr('title');
+        try {
+            $seoTitle = $crawler->filter('title')->text();
+        } catch (\Exception $e) {
+            $seoTitle = '';
+        }
+        try {
+            $seoDescription = $crawler->filter('meta[name=description]')->attr('content');
+        } catch (\Exception $e) {
+            $seoDescription = '';
+        }
+        $techs = $crawler->filter('div.content-text-block')->html();
+        $documents = $crawler->filter('div.b-item-doc >div.b-item-doc-column')->each(
+            function (Crawler $document) {
+                $document_name = $document->filter('div.column_title')->text();
+                $document_link = $document->filter('div.download-file >a')->first()->link()->getUri();
+                return [
+                    $document_name => $document_link
+                ];
+            }
+        );
+        $certificates = $crawler->filter('div.download-file >a')->each(
+            function (Crawler $certificate) {
+                $certificate_name = $certificate->text();
+                $certificate_link = $certificate->link()->getUri();
+                return [
+                    $certificate_name => $certificate_link
+                ];
+            }
+        );
+        dd($certificates);
     }
 }
