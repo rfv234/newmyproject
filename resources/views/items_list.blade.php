c1fa0dfd0b5be4170dae546d19fb46b64560b911
rfv234 <gromdusha154@gmail.com>
2022-12-23 21:24:56 +0700

    parser design

--- resources/views/items_list.blade.php
+++ resources/views/items_list.blade.php
@@ -1,3 +1,24 @@
+<style>
+    #object {
+        margin: 20px;
+        border: solid 2px;
+    }
+
+    #name {
+        text-align: center;
+    }
+</style>
 @foreach($items as $item)
-    <p>{{$item->name}}</p>
+    <div id="object">
+        <h3 id="name">{{$item->name}}</h3>
+        <span>{{$item->description}}</span><br>
+        <p>{{$item->category}}</p><br>
+        <p>{{$item->seoTitle}}</p><br>
+        <p>{{$item->seoDescription}}</p><br>
+        <p>{{$item->techs}}</p><br>
+        <p>{{$item->documents}}</p>
+        <hr>
+        <p>{{$item->certificates}}</p><br>
+        <p>{{$item->garanties}}</p><br>
+    </div>
 @endforeach
